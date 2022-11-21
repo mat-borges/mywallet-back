317a356040dfcd69d0f0a73fe6f4cacf81da78a1
Mateus Borges <borgesm.work@gmail.com>
2022-11-21 12:00:58 -0300

    fix!: add date to putWallet

--- src/controllers/walletController.js
+++ src/controllers/walletController.js
@@ -1,5 +1,12 @@
 import { sessionsCollection, walletsCollection } from '../db/db.js';
 
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone.js';
+import utc from 'dayjs/plugin/utc.js';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 export async function getWallet(req, res) {
 	const { token } = res.locals;
 
@@ -32,9 +39,16 @@ export async function postWallet(req, res) {
 	try {
 		const session = await sessionsCollection.findOne({ token });
 		const userId = session?.userId;
+		const now = dayjs();
+		const dateBrazil = dayjs.tz(now).tz('America/Sao_Paulo').format('DD/MM');
 
-		await walletsCollection.insertOne({ userId: userId, description, value, type });
-
+		await walletsCollection.insertOne({
+			userId: userId,
+			description,
+			value,
+			type,
+			date: dateBrazil,
+		});
 		res.sendStatus(200);
 	} catch (err) {
 		console.log(err);
